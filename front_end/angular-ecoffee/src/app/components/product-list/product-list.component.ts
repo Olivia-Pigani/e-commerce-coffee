1bb544fa09326a941c3d07f5c051dfe38826eb92
Olivia-Pigani <piganiolivia@gmail.com>
2024-01-21 16:13:23 +0100

    ProductService injected in product list component

---
+++ front_end/angular-ecoffee/src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../common/product';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent implements OnInit{
+
+  products : Product[] = []
+  constructor(private productService : ProductService){}
+
+  ngOnInit(): void {
+    
+  }
+
+}
